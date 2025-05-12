d79fac31b9b4a00a3c7d03b39a20fb75c9a16e06
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2025-05-12 17:40:38 -0300

    update config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -9,7 +10,12 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    host: "0.0.0.0",
+  },
   integrations: [
     react(),
     {
